5ebf0fcfa2f359f58276f35d248ba4b78e16f4bc
Ivan Ayopela <vanayopela3@gmail.com>
2025-01-15 18:38:50 +0800

    feat: enhance App component with dynamic background color and layout adjustments

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import "@/global.css";
+import "./global.css";
+
+import { useColorScheme } from "react-native";
 import { GluestackUIProvider } from "@/src/components/ui/gluestack-ui-provider";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthStack } from "./src/navigation/AuthStack";
@@ -11,33 +13,33 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <GluestackUIProvider mode="light">
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Auth"
-            options={{ headerShown: false }}
-            component={AuthStack}
-          />
-          <Stack.Screen
-            name="Dashboard"
-            options={{ headerShown: false }}
-            component={DashboardStack}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar style="auto" />
-    </GluestackUIProvider>
+    <View
+      style={{
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        backgroundColor: useColorScheme() === "dark" ? "#202020" : "#FFFFFF",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <GluestackUIProvider mode="system">
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Auth"
+              options={{ headerShown: false }}
+              component={AuthStack}
+            />
+            <Stack.Screen
+              name="Dashboard"
+              options={{ headerShown: false }}
+              component={DashboardStack}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <StatusBar style="auto" />
+      </GluestackUIProvider>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
