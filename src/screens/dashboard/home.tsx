79b16e1ad885e29a58ec068606668851085a9a68
Ivan Ayopela <vanayopela3@gmail.com>
2025-01-16 17:50:34 +0800

    feat: add logout button to Home screen with navigation to SignIn

--- src/screens/dashboard/home.tsx
+++ src/screens/dashboard/home.tsx
@@ -1,11 +1,21 @@
+import { Button, ButtonText } from "@/src/components/ui/button";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Home() {
+// WIP
+// @ts-ignore
+export default function Home(props: { navigation: any }) {
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={() => {
+          props.navigation.replace("Auth", { screen: "SignIn" });
+        }}
+      >
+        <ButtonText>Log out</ButtonText>
+      </Button>
     </View>
   );
 }
